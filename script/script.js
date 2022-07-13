24fa715c292ce51cce3bcfdb17875dc6199c2578
Elena Berestok <=>
2022-07-14 00:09:52 +0300

    item2

--- script/script.js
+++ script/script.js
@@ -1,20 +1,53 @@
 // Пункт 1
 
-const countTrue = (arr) =>{
+// const countTrue = (arr) =>{
 
-    let sum = 0;
+//     let sum = 0;
+
+//     for (let elem of arr) {
+
+//         if (elem === true) {
+//             sum += 1;
+//         } 
+//     }
+
+//     return sum;
+// }
+
+// let sumArray = countTrue([true, false, false, true, false]);
+// // let sumArray = countTrue([false, false, false, false]);
+// // let sumArray = countTrue([]);
+// console.log(`sumArray`, sumArray)
+
+// Пункт 2
+
+const getOccurrencesCount = (arr) => {
+
+    let sumEntry = {}
 
     for (let elem of arr) {
-
-        if (elem === true) {
-            sum += 1;
-        } 
+        if (!sumEntry[elem]) {
+            sumEntry[elem] = 1
+        } else {
+            sumEntry[elem] += 1
+        }
     }
 
-    return sum;
+    console.log(`sumEntry`, sumEntry)
+
+    return sumEntry;
 }
 
-let sumArray = countTrue([true, false, false, true, false]);
-// let sumArray = countTrue([false, false, false, false]);
-// let sumArray = countTrue([]);
-console.log(`sumArray`, sumArray)
+// let occurrencesCount = getOccurrencesCount(["a", "v", "a", "b", "b"]);
+let occurrencesCount = getOccurrencesCount([
+    "apples",
+    "oranges",
+    "pears",
+    "pears",
+    "apples",
+    "oranges",
+    "oranges",
+    "pears",
+  ]);
+
+console.log(`occurrencesCount`, occurrencesCount)
