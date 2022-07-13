d2064c6288983157f39d8eb760384627d0cdfb8d
Elena Berestok <=>
2022-07-14 00:31:18 +0300

    item3

--- script/script.js
+++ script/script.js
@@ -1,23 +1,26 @@
 // Пункт 1
 
-// const countTrue = (arr) =>{
+const countTrue = (arr) =>{
 
-//     let sum = 0;
+    let sum = 0;
 
-//     for (let elem of arr) {
+    for (let elem of arr) {
 
-//         if (elem === true) {
-//             sum += 1;
-//         } 
-//     }
+        if (elem === true) {
+            sum += 1;
+        } 
+    }
 
-//     return sum;
-// }
+    return sum;
+}
 
-// let sumArray = countTrue([true, false, false, true, false]);
-// // let sumArray = countTrue([false, false, false, false]);
-// // let sumArray = countTrue([]);
-// console.log(`sumArray`, sumArray)
+let sumArray = countTrue([true, false, false, true, false]);
+// let sumArray = countTrue([false, false, false, false]);
+// let sumArray = countTrue([]);
+console.log(`sumArray`, sumArray)
+
+
+
 
 // Пункт 2
 
@@ -38,16 +41,48 @@ const getOccurrencesCount = (arr) => {
     return sumEntry;
 }
 
-// let occurrencesCount = getOccurrencesCount(["a", "v", "a", "b", "b"]);
-let occurrencesCount = getOccurrencesCount([
-    "apples",
-    "oranges",
-    "pears",
-    "pears",
-    "apples",
-    "oranges",
-    "oranges",
-    "pears",
-  ]);
+let occurrencesCount = getOccurrencesCount(["a", "v", "a", "b", "b"]);
+// let occurrencesCount = getOccurrencesCount([
+//     "apples",
+//     "oranges",
+//     "pears",
+//     "pears",
+//     "apples",
+//     "oranges",
+//     "oranges",
+//     "pears",
+//   ]);
 
 console.log(`occurrencesCount`, occurrencesCount)
+
+
+
+
+// Пункт 3
+
+let findExcess = (arr) => {
+
+    let oddNumber = [];
+    let evenNumber = [];
+
+    for (let i of arr) {
+        if (i % 2 == 0) {
+            evenNumber.push(i)
+        } else {
+            oddNumber.push(i)
+        }
+        
+    } 
+
+    if (oddNumber.length > evenNumber.length){
+        return evenNumber[0]
+    } else {
+        return oddNumber[0]
+    }
+}
+
+// let unnecessaryNumber = findExcess([0, 1, 2]);
+// let unnecessaryNumber = findExcess([1, 2, 3]);
+// let unnecessaryNumber = findExcess([0, 0, 3, 0, 0]);
+let unnecessaryNumber = findExcess([1, 1, 0, 1, 1]);
+console.log(`unnecessaryNumber`, unnecessaryNumber)
